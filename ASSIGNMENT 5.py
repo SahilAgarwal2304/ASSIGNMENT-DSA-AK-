2adce16463cc46ae5c32bd8caf31325dc772ce92
SahilAgarwal2304 <166193356+SahilAgarwal2304@users.noreply.github.com>
2025-01-13 21:05:25 +0530

    Update ASSIGNMENT 5.py

--- ASSIGNMENT 5.py
+++ ASSIGNMENT 5.py
@@ -1,25 +1,25 @@
-sum=0
-temp=0
-for i in range (5):
-    marks=int(input("Enter marks:"))
-    if marks>100 or marks<0:
+sum = 0
+temp = 0
+n = int(input("Enter the number of subjects: "))
+for i in range(n):
+    marks = int(input(f"Enter marks for subject {i + 1}: "))
+    if marks > 100 or marks < 0:
         print("Enter valid marks!")
-        i-=1
-    if marks<40:
-        temp=1
-    sum+=marks
-if temp==0:
-    percent=sum/5
-    print("Your Percentage=",percent)
-    if percent<40:
+        i -= 1
+        continue
+    if marks < 40:
+        temp = 1
+    sum += marks
+if temp == 0:
+    percent = sum / n
+    print("Your Percentage =", percent)
+    if percent < 40:
         print("FAIL")
-    elif percent<60:
-        print("PASS-iii.Div")
-    elif percent<80:
-        print("PASS-ii.Div")
-    elif percent<100:
-        print("PASS-i.Div")
-    else:
-        print("Enter Valid Marks")
+    elif percent < 60:
+        print("PASS - III Div.")
+    elif percent < 80:
+        print("PASS - II Div.")
+    elif percent <= 100:
+        print("PASS - I Div.")
 else:
     print("FAIL")
