3acc4be51d9968ee560df034b97070e0ab063f5d
SahilAgarwal2304 <166193356+SahilAgarwal2304@users.noreply.github.com>
2025-02-10 11:01:00 +0530

    Update ASSIGNMENT 01.js

--- ASSIGNMENT 01.js
+++ ASSIGNMENT 01.js
@@ -1,8 +1,13 @@
-var cp=prompt("Enter Cost Price:");
-var sp=prompt("Enter Selling Price:");
-if(sp-cp>0)
-    console.log("Your Profit is:",sp-cp);
-else if(sp==cp)
-    console.log("You dont have any pofit or loss(0)");
-else
-    console.log("Your Loss is:",cp-sp);
+var cp = prompt("Enter Cost Price:");
+if (cp < 0) {
+    console.log("Cost Price cannot be negative.");
+} else {
+    var sp = prompt("Enter Selling Price:");
+    if (sp - cp > 0) {
+        console.log("Your Profit is:", sp - cp);
+    } else if (sp == cp) {
+        console.log("You don't have any profit or loss (0)");
+    } else {
+        console.log("Your Loss is:", cp - sp);
+    }
+}
