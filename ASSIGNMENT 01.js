d12a2ba1a530259772489c3e9f16dea667fd3176
SahilAgarwal2304 <166193356+SahilAgarwal2304@users.noreply.github.com>
2025-02-12 12:11:23 +0530

    Update ASSIGNMENT 01.js

--- ASSIGNMENT 01.js
+++ ASSIGNMENT 01.js
@@ -1,13 +1,23 @@
 var cp = prompt("Enter Cost Price:");
-if (cp < 0) {
+cp = parseFloat(cp);
+
+if (isNaN(cp)) {
+    console.log("Invalid input. Cost Price must be a valid number.");
+} else if (cp < 0) {
     console.log("Cost Price cannot be negative.");
 } else {
     var sp = prompt("Enter Selling Price:");
-    if (sp - cp > 0) {
-        console.log("Your Profit is:", sp - cp);
-    } else if (sp == cp) {
-        console.log("You don't have any profit or loss (0)");
+    sp = parseFloat(sp);
+
+    if (isNaN(sp)) {
+        console.log("Invalid input. Selling Price must be a valid number.");
     } else {
-        console.log("Your Loss is:", cp - sp);
+        if (sp - cp > 0) {
+            console.log("Your Profit is:", sp - cp);
+        } else if (sp == cp) {
+            console.log("You don't have any profit or loss (0)");
+        } else {
+            console.log("Your Loss is:", cp - sp);
+        }
     }
 }
