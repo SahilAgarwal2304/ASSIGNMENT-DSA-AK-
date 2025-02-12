fd3d753a475a02d0cafa7c566551b12051ca3510
SahilAgarwal2304 <166193356+SahilAgarwal2304@users.noreply.github.com>
2025-02-12 12:14:45 +0530

    Update ASSIGNMENT 04.py

--- ASSIGNMENT 04.py
+++ ASSIGNMENT 04.py
@@ -1,4 +1,9 @@
-import math
-r=int(input("Enter Radius:"))
-print("Area=",math.pi*(r**2))
-print("Circumference=",2*math.pi*r)
+r = input("Enter Radius:")
+
+if r.isdigit():
+    r = int(r)
+    import math
+    print("Area=", math.pi * (r**2))
+    print("Circumference=", 2 * math.pi * r)
+else:
+    print("Invalid input! Please enter a valid integer for radius.")
