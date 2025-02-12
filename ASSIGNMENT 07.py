4b54fb87f98f0bb3f8681e8667781da0960e0cb1
SahilAgarwal2304 <166193356+SahilAgarwal2304@users.noreply.github.com>
2025-02-12 12:19:40 +0530

    Update ASSIGNMENT 07.py

--- ASSIGNMENT 07.py
+++ ASSIGNMENT 07.py
@@ -1,13 +1,18 @@
 lst = []
 print("Enter 3 nos:")
 for i in range(3):
-    ele = int(input())
-    lst.append(ele)
+    ele = input()
+    if ele.isdigit():
+        lst.append(int(ele))
+    else:
+        print("Invalid input! Please enter a valid integer.")
+        break
 
-max_val = lst[0]
-
-for num in lst:
-    if num > max_val:
-        max_val = num
-
-print("The maximum number is:", max_val)
+if len(lst) == 3:
+    max_val = lst[0]
+    for num in lst:
+        if num > max_val:
+            max_val = num
+    print("The maximum number is:", max_val)
+else:
+    print("Input was not valid. Exiting.")
